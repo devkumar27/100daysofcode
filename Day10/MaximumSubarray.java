3f6a7406133e8c824081e6bab5b03fb865d84204
Dev Kumar <75902815+devkumar27@users.noreply.github.com>
2023-05-16 22:47:38 +0530

    Create MaximumSubarray.java

---
+++ Day10/MaximumSubarray.java
@@ -0,0 +1,17 @@
+// LC53 - https://leetcode.com/problems/maximum-subarray/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+
+        for(int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            maxSum = Math.max(maxSum, sum);
+            if(sum<0) {
+                sum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
