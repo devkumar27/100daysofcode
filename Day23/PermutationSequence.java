3ea3cbcbd22a1533d815e5deab06733d14f3542a
Dev Kumar <75902815+devkumar27@users.noreply.github.com>
2023-05-26 23:28:47 +0530

    Create PermutationSequence.java

---
+++ Day23/PermutationSequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        int fact = 1;
+        List<Integer> nums = new ArrayList<>();
+        for(int i=1; i<n; i++) {
+            fact = fact * i;
+            nums.add(i);
+        }
+        nums.add(n);
+        k--;
+        String res = "";
+        while(true) {
+            res = res + nums.get(k/fact);
+            nums.remove(k/fact);
+            if(nums.size() == 0) {
+                break;
+            }
+            k = k % fact;
+            fact = fact / nums.size();
+        }
+        return res;
+    }
+}
